6e0ffb61326981c662733f859b3fb222a1c13cd3
Sumit Sureka <sumitsureka300@gmail.com>
2020-05-12 20:05:44 +0530

    Updates

---
+++ precache-manifest.a3f5d2fec8f0809a5494de368be36a2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371fbc788d7c2c6b2015ba91fc16c6b7",
+    "url": "/robofriends1/index.html"
+  },
+  {
+    "revision": "aabc5e357dd431670b4d",
+    "url": "/robofriends1/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5e57c81f8b32bdf4e8df",
+    "url": "/robofriends1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aabc5e357dd431670b4d",
+    "url": "/robofriends1/static/js/2.5c85da11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends1/static/js/2.5c85da11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e57c81f8b32bdf4e8df",
+    "url": "/robofriends1/static/js/main.9b8a1417.chunk.js"
+  },
+  {
+    "revision": "c707cda9963d661e74c6",
+    "url": "/robofriends1/static/js/runtime-main.9c749947.js"
+  }
+]);
